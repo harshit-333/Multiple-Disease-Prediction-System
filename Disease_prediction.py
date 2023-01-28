31a173b1548c8259848cf01ace9c083debb369b0
harshit-333 <91932750+harshit-333@users.noreply.github.com>
2023-01-28 16:43:37 +0530

    Update Disease_prediction.py

--- Disease_prediction.py
+++ Disease_prediction.py
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('D:/NITK Academics/IRIS/5th Semester/IT303_Software Engineering/Project/saved models/diabetes_model.sav','rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
 
-heart_disease_model = pickle.load(open('D:/NITK Academics/IRIS/5th Semester/IT303_Software Engineering/Project/saved models/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('D:/NITK Academics/IRIS/5th Semester/IT303_Software Engineering/Project/saved models/parkinsons_model.sav','rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav','rb'))
 
 
 # sidebar for navigation
